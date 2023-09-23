d46a974d39f1f2a497d68b62c1911ecea4bfc9f3
Gregar <126393669+mrd010@users.noreply.github.com>
2023-09-23 20:07:57 +0330

    Add start playing function

--- script_v2.js
+++ script_v2.js
@@ -1,17 +1,32 @@
 const startBtn = document.querySelector("#start");
 const resetBtn = document.querySelector("#reset");
 
-const playButtons = document.querySelectorAll(".btn");
-const rockBtn = document.querySelector("#rock");
-const paperBtn = document.querySelector("#paper");
-const scissorBtn = document.querySelector("#scissor");
+const playButtons = document.querySelectorAll(".btn"); // play buttons Rock Paper ....
 
-startBtn.addEventListener("click", startPlaying);
+startBtn.addEventListener("click", startPlaying); // Start button will start the game
 
-function startPlaying() {
-  resetPlaying();
-}
+resetBtn.addEventListener("click", resetPlaying); // Reset button will reset game state
+
+let userScore = 0;
+let cpuScore = 0;
 
 function resetPlaying() {
-  playButtons.forEach((btn) => btn.removeAttribute("disabled"));
+  // disable play buttons
+  playButtons.forEach((btn) => btn.setAttribute("disabled", ""));
+  // reset scores and info
+  const resetElements = document.querySelectorAll(".empty-first");
+  resetElements.forEach((element) => (element.textContent = "-"));
 }
+
+function startPlaying() {
+  // first reset game state before play
+  resetPlaying();
+
+  // enable play button to play
+  playButtons.forEach((btn) => btn.removeAttribute("disabled"));
+
+  // play a round when a play button clicked
+  playButtons.forEach((btn) => btn.addEventListener("click", playRound));
+}
+
+function playRound() {}
